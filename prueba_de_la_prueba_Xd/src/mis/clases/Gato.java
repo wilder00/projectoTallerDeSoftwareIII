629a7cabdaa695735a0dfd897c59f27bd263ca67
Wilder Trujillo <nadie437@gmail.com>
2018-10-27 19:57:46 -0500

    primer commit

---
+++ prueba_de_la_prueba_Xd/src/mis/clases/Gato.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mis.clases;
+
+/**
+ *
+ * @author Wilder
+ */
+public class Gato {
+    private String color;
+    private String nombre;
+    private int edad;
+    
+}
